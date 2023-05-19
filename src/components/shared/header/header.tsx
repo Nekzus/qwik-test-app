1a240b7f56275c59f39997d4f8de1deadaf85ad1
Nekzus <85507589+Nekzus@users.noreply.github.com>
2023-05-18 21:11:57 -0300

    Primer commit

---
+++ src/components/shared/header/header.tsx
@@ -0,0 +1,27 @@
+import { QwikLogo } from "../../icons/qwik";
+import { component$ } from "@builder.io/qwik";
+import styles from "./header.module.css";
+
+export default component$(() => {
+  return (
+    <header class={styles.header}>
+      <div class={["container", styles.wrapper]}>
+        <div class={styles.logo}>
+          <a href="/" title="qwik">
+            <QwikLogo height={50} width={143} />
+          </a>
+        </div>
+        <ul>
+          <li>
+            <a
+              href="https://qwik.builder.io/docs/components/overview/"
+              target="_blank"
+            >
+              Documentación
+            </a>
+          </li>
+        </ul>
+      </div>
+    </header>
+  );
+});
