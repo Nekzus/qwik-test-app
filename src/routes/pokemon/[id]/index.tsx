95bc6b191197b4c43ff7d315b82a2a7308dc74e1
Nekzus <85507589+Nekzus@users.noreply.github.com>
2023-07-13 23:37:22 -0300

    Commit de control

--- src/routes/pokemon/[id]/index.tsx
+++ src/routes/pokemon/[id]/index.tsx
@@ -1,14 +1,25 @@
 import { PokemonImage } from "../../../components/pokemons/pokemon-image";
 import { component$ } from "@builder.io/qwik";
-import { useLocation } from "@builder.io/qwik-city";
+import { routeLoader$ } from "@builder.io/qwik-city";
+
+export const usePokemonId = routeLoader$<number>(({ params, redirect }) => {
+  const id = Number(params.id);
+  if (isNaN(id)) redirect(301, "/");
+  if (id <= 0) redirect(301, "/");
+  if (id > 1000) redirect(301, "/");
+
+  return id;
+});
 
 export default component$(() => {
-  const location = useLocation();
+  // const location = useLocation();
+  const pokemonId = usePokemonId();
 
   return (
     <>
-      <span class="text-5xl">Pokemon: {location.params.id} </span>
-      <PokemonImage id={location.params.id} />
+      {/* <span class="text-5xl">Pokemon: {location.params.id} </span> */}
+      <span class="text-5xl">Pokemon: {pokemonId} </span>
+      <PokemonImage id={pokemonId.value} isVisible />
     </>
   );
 });
--- src/routes/pokemons/list-ssr/index.tsx
+++ src/routes/pokemons/list-ssr/index.tsx
@@ -1,8 +1,41 @@
-import type { DocumentHead } from "@builder.io/qwik-city";
+import { Link, type DocumentHead, routeLoader$ } from "@builder.io/qwik-city";
 import { component$ } from "@builder.io/qwik";
+import type {
+  BasicPokemonInfo,
+  PokemonListResponse,
+} from "../../../interfaces";
+
+export const usePokemonList = routeLoader$<BasicPokemonInfo[]>(async () => {
+  const resp = await fetch(
+    "https://pokeapi.co/api/v2/pokemon?limit=10&offset=10"
+  );
+  const data = (await resp.json()) as PokemonListResponse;
+  return data.results;
+});
 
 export default component$(() => {
-  return <span>Hola Mundo - SSR</span>;
+  const pokemons = usePokemonList();
+  return (
+    <>
+      <div class="flex flex-col">
+        <span class="my-5 text-5xl">Status</span>
+        <span>Página actual: xxxxx</span>
+        <span>Está cargando página</span>
+      </div>
+
+      <div class="mt-10">
+        <Link class="btn btn-primary mr-2">Anteriores</Link>
+        <Link class="btn btn-primary mr-2">Siguientes</Link>
+      </div>
+      <div class="grid grid-cols-6 mt-5">
+        {pokemons.value.map(({ name }) => (
+          <div key={name} class="m-5 flex flex-col justify-center items-center">
+            <span class="capitalize">{name}</span>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 });
 
 export const head: DocumentHead = {
